85c8838d8975668af07da77f1056fc122a6aff22
Mike Stone <stonemj@gmail.com>
2012-05-26 15:44:28 -0700

    Finish compile and the specs for it.

--- lib/nginxtra/actions/compile.rb
+++ lib/nginxtra/actions/compile.rb
@@ -1,18 +1,31 @@
 module Nginxtra
   module Actions
+    # The Nginxtra::Actions::Compile class encapsulates compiling
+    # nginx so it is ready with the specified compile options.
     class Compile
       def initialize(thor, config)
         @thor = thor
         @config = config
       end
 
+      # Run the full compilation of nginx.
       def compile
         configure
+        make
+        make "install"
       end
 
+      # Configure nginx with the specified compile options.
       def configure
         @thor.inside Nginxtra::Config.src_dir do
           @thor.run "./configure --prefix=#{Nginxtra::Config.build_dir} #{@config.compile_options}"
+        end
+      end
+
+      # Run make against the configured nginx.
+      def make(*args)
+        @thor.inside Nginxtra::Config.src_dir do
+          @thor.run ["make", *args].join(" ")
         end
       end
     end
--- spec/compile_spec.rb
+++ spec/compile_spec.rb
@@ -3,12 +3,15 @@ require "spec_helper"
 describe Nginxtra::Actions::Compile do
   let(:thor_mock) { Object.new }
   let(:config_mock) { Object.new }
+  let(:src_dir) { File.absolute_path File.expand_path("../../src/nginx", __FILE__) }
+  let(:build_dir) { File.absolute_path File.expand_path("../../build/nginx", __FILE__) }
 
-  it "configures based on the passed in config" do
-    thor_mock.should_receive(:inside).with(File.absolute_path(File.expand_path("../../src/nginx", __FILE__))).and_yield
-    thor_mock.should_receive(:run).with("./configure --prefix=#{File.absolute_path File.expand_path("../../build/nginx", __FILE__)} --option1 --option2")
+  it "compiles based on the passed in config" do
+    thor_mock.stub(:inside).with(src_dir).and_yield
+    thor_mock.should_receive(:run).with("./configure --prefix=#{build_dir} --option1 --option2")
+    thor_mock.should_receive(:run).with("make")
+    thor_mock.should_receive(:run).with("make install")
     config_mock.should_receive(:compile_options).and_return("--option1 --option2")
-    compile = Nginxtra::Actions::Compile.new thor_mock, config_mock
-    compile.configure
+    Nginxtra::Actions::Compile.new(thor_mock, config_mock).compile
   end
 end
