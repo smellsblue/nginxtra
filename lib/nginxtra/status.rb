27eff19a2510c1b0a5acd2ac3efa1639876ead91
Mike Stone <stonemj@gmail.com>
2012-05-28 01:45:32 -0700

    Add comments and finish the implementation.

--- lib/nginxtra/status.rb
+++ lib/nginxtra/status.rb
@@ -5,14 +5,22 @@ module Nginxtra
   # when the last time nginx was compiled and with what options.
   class Status
     @@status = nil
+    # The name of the file that stores the state.
     FILENAME = ".nginxtra_status".freeze
 
     class << self
+      # Retrieve an option from the state stored in the filesystem.
+      # This will first load the state from the .nginxtra_status file
+      # in the root of the nginxtra gem directory, if it has not yet
+      # been loaded.
       def[](option)
         load!
         @@status[option]
       end
 
+      # Store an option to the status state.  This will save out to
+      # the filesystem immediately after storing this option.  The
+      # return value will be the value stored to the given option key.
       def[]=(option, value)
         load!
         @@status[option] = value
@@ -20,19 +28,29 @@ module Nginxtra
         value
       end
 
+      private
+      # Load the status state, if it hasn't yet been loaded.  If there
+      # is no file yet existing, then the state is simply initialized
+      # to an empty hash (and it is NOT stored to the filesystem til
+      # the first write).
       def load!
         return if @@status
 
-        if File.exists? File.join(Nginxtra::Config.base_dir, FILENAME)
-          # TODO
+        if File.exists? path
+          @@status = YAML.load File.read path
         else
           @@status = {}
         end
       end
 
-      private
+      # The full path to the file with the state.
+      def path
+        File.join Nginxtra::Config.base_dir, FILENAME
+      end
+
+      # Save the current state to disk.
       def save!
-        YAML.dump @@status
+        File.write path, YAML.dump(@@status)
       end
     end
   end
--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -2,10 +2,11 @@ require "spec_helper"
 
 describe Nginxtra::Status do
   let(:base_dir) { File.absolute_path File.expand_path("../..", __FILE__) }
+  let(:status_file) { File.join(base_dir, ".nginxtra_status") }
   before { Nginxtra::Status.class_variable_set :@@status, nil }
 
   describe "with no file to load" do
-    before { File.should_receive(:exists?).with(File.join(base_dir, ".nginxtra_status")).and_return(false) }
+    before { File.should_receive(:exists?).with(status_file).and_return(false) }
 
     it "defaults options to nil" do
       Nginxtra::Status[:last_compile_options].should == nil
@@ -14,14 +15,42 @@ describe Nginxtra::Status do
 
     it "saves out to a file when options are stored" do
       YAML.should_receive(:dump).with({ :last_compile_options => "--some-option" }).and_return("The YAML dumped content")
+      File.should_receive(:write).with(status_file, "The YAML dumped content")
       Nginxtra::Status[:last_compile_options] = "--some-option"
       Nginxtra::Status[:last_compile_options].should == "--some-option"
     end
   end
 
   describe "with a file to load" do
-    it "defaults missing options to nil"
-    it "returns values stored in the file"
-    it "saves out to a file when options are stored"
+    before do
+      File.should_receive(:exists?).with(status_file).and_return(true)
+      File.should_receive(:read).with(status_file).and_return(YAML.dump({ :last_compile_options => "--original-option", :other_option => "something" }))
+    end
+
+    it "defaults missing options to nil" do
+      Nginxtra::Status[:missing_option].should == nil
+    end
+
+    it "returns values stored in the file" do
+      Nginxtra::Status[:last_compile_options].should == "--original-option"
+      Nginxtra::Status[:other_option].should == "something"
+    end
+
+    it "saves out to a file when new options are stored" do
+      YAML.should_receive(:dump).with({ :last_compile_options => "--original-option", :other_option => "something", :new_option => "new value" }).and_return("The YAML dumped content")
+      File.should_receive(:write).with(status_file, "The YAML dumped content")
+      Nginxtra::Status[:new_option] = "new value"
+      Nginxtra::Status[:new_option].should == "new value"
+      Nginxtra::Status[:last_compile_options].should == "--original-option"
+      Nginxtra::Status[:other_option].should == "something"
+    end
+
+    it "saves out to a file when old options are updated" do
+      YAML.should_receive(:dump).with({ :last_compile_options => "--some-option", :other_option => "something" }).and_return("The YAML dumped content")
+      File.should_receive(:write).with(status_file, "The YAML dumped content")
+      Nginxtra::Status[:last_compile_options] = "--some-option"
+      Nginxtra::Status[:last_compile_options].should == "--some-option"
+      Nginxtra::Status[:other_option].should == "something"
+    end
   end
 end
