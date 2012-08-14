b82398cc567d7930d267c85a491df502374974ba
Mike Stone <stonemj@gmail.com>
2012-08-14 00:05:40 -0700

    Add a new test app to help running manual tests.

---
+++ test/app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '5052f541f9858a650753c9ddcf1e81068dff30ae50b7e6e9c02e40cbb5f562de64afd32ddb9d7d1412659004f08781a8e06d9b1d10f39c7c0f8967903a29b718'
